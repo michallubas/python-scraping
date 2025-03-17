75a4d94259e890519dbae5e6fa7746b676b85fed
michal.lubas <michal.lubas@damovo.com>
2025-03-17 23:03:06 +0100

    Task1

--- first_part/src.py
+++ first_part/src.py
@@ -1,1 +1,30 @@
 # Your code here
+some_list = ['milk', 'catalog', 'c+', 'python', 'cat', 'dog']
+
+
+def count(search_word, _list):
+    counter = 0
+    for word in _list:
+        if search_word in word:
+            counter += 1
+    return counter
+
+
+def longest_word(some_list):
+    longest_seq = {"word": '', "count": 0}
+
+    for word in some_list:
+
+        long_internal_seq = ''
+
+        for letter in word:
+
+            long_internal_seq += letter
+
+            temp_counter = count(long_internal_seq, some_list)
+
+            if any([len(long_internal_seq) > len(longest_seq["word"]), temp_counter > longest_seq["count"]]):
+                longest_seq['word'] = long_internal_seq
+                longest_seq['count'] = temp_counter
+
+    return longest_seq["word"]
--- first_part/test_src.py
+++ first_part/test_src.py
@@ -1,1 +1,10 @@
 # Your tests here
+
+import pytest
+from first_part.src import longest_word
+
+def test_logest_word():
+    test_array_1 = ['milk', 'catalog', 'c+', 'python', 'cat', 'dog']
+    test_array_1_result = 'cat'
+
+    assert longest_word(test_array_1) == test_array_1_result
